9bc9415e592938b840743a04b0379a087a344e66
MikeLeon23 <793766131@qq.com>
2020-05-13 16:48:01 +0800

    家政手机端首页

---
+++ Fundamental/不凡作业/家政移动端/js/data.js
@@ -0,0 +1,105 @@
+var serviceArr = [
+    {
+        item: "日常保洁",
+        color: "linear-gradient(0deg, #F4A77A 5%, #EB6B2B 100%);"
+    },
+    {
+        item: "空房开荒",
+        color: "linear-gradient(0deg, #F380BA 0%, #D74B8B 99%);"
+    },
+    {
+        item: "专业擦玻璃",
+        color: "linear-gradient(0deg, #FB9985 0%, #E55239 94%);"
+    },
+    {
+        item: "厨房熏蒸",
+        color: "linear-gradient(-1deg, #C3D46D 3%, #A1C021 97%);"
+    },
+    {
+        item: "地板打蜡",
+        color: "linear-gradient(0deg, #F2C695 0%, #ED933E 100%);"
+    },
+    {
+        item: "地毯清洗",
+        color: "linear-gradient(-1deg, #A4B8F4 1%, #657ED8 99%);"
+    },
+    {
+        item: "窗帘清洗",
+        color: "linear-gradient(0deg, #F4A77A 5%, #EB6B2B 100%);"
+    },
+    {
+        item: "家居保养",
+        color: "linear-gradient(-1deg, #FAA8CC 6%, #E76198 98%);"
+    },
+    {
+        item: "沙发保养",
+        color: "linear-gradient(-1deg, #BEB8F8 0%, #7C72EF 97%);"
+    },
+    {
+        item: "卫生间消毒",
+        color: "linear-gradient(-1deg, #C3E582 4%, #86B935 98%);"
+    },
+    {
+        item: "日常保洁",
+        color: "linear-gradient(0deg, #F4A77A 5%, #EB6B2B 100%);"
+    },
+    {
+        item: "空房开荒",
+        color: "linear-gradient(0deg, #F380BA 0%, #D74B8B 99%);"
+    },
+    {
+        item: "专业擦玻璃",
+        color: "linear-gradient(0deg, #FB9985 0%, #E55239 94%);"
+    },
+    {
+        item: "厨房熏蒸",
+        color: "linear-gradient(-1deg, #C3D46D 3%, #A1C021 97%);"
+    },
+    {
+        item: "地板打蜡",
+        color: "linear-gradient(0deg, #F2C695 0%, #ED933E 100%);"
+    },
+    {
+        item: "地毯清洗",
+        color: "linear-gradient(-1deg, #A4B8F4 1%, #657ED8 99%);"
+    },
+    {
+        item: "窗帘清洗",
+        color: "linear-gradient(0deg, #F4A77A 5%, #EB6B2B 100%);"
+    },
+    {
+        item: "家居保养",
+        color: "linear-gradient(-1deg, #FAA8CC 6%, #E76198 98%);"
+    },
+    {
+        item: "沙发保养",
+        color: "linear-gradient(-1deg, #BEB8F8 0%, #7C72EF 97%);"
+    },
+    {
+        item: "卫生间消毒",
+        color: "linear-gradient(-1deg, #C3E582 4%, #86B935 98%);"
+    }
+];
+
+var popularArr = [
+    {
+        title: "包月保洁",
+        description: "轻松一月，保洁无忧",
+        hotFlag: true
+    },
+    {
+        title: "厨房深度清洁",
+        description: "新的生活，新的厨房",
+        hotFlag: false
+    },
+    {
+        title: "卫生间深度清洁",
+        description: "美好的一天从早开始",
+        hotFlag: false
+    },
+    {
+        title: "四小时擦玻璃",
+        description: "隔窗也能看风景了",
+        hotFlag: true
+    },
+];
