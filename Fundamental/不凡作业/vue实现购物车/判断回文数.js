fb272bbec6752270f410b3373270a5945f359d62
MikeLeon23 <793766131@qq.com>
2020-06-11 20:42:42 +0800

    nothing special

--- Fundamental/不凡作业/vue实现购物车/判断回文数.js
+++
@@ -1,9 +0,0 @@
-var isPalindrome = function(x) {
-    if(x < 0){
-        return false;
-    }else{
-        var a = x + "";
-        var b = a.split("").reverse().join("");
-        return b==a;
-    }
-};
